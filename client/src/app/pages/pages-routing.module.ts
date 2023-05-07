59262a785af8c47e0cfccb87cc021a41299ca95d
Joel7Anthony <anto777molina777@gmail.com>
2023-05-07 13:08:17 -0500

    pegues

--- client/src/app/pages/pages-routing.module.ts
+++ client/src/app/pages/pages-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
-import { NoFoundPageComponent } from '../noFoundPage/no-found-page.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProductsComponent } from './products/products.component';
