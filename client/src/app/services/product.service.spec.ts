ad5dc1f8c959632d68432449f27badec5c7ae634
Joel7Anthony <anto777molina777@gmail.com>
2023-05-11 20:51:36 -0500

    cambio

--- client/src/app/services/product.service.spec.ts
+++ client/src/app/services/product.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProductService } from './product.service';
+import { ProductHttpService } from './product.service';
 
 describe('ProductService', () => {
-  let service: ProductService;
+  let service: ProductHttpService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
+    service = TestBed.inject(ProductHttpService);
   });
 
   it('should be created', () => {
